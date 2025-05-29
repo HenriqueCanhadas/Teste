84bd9e791cfed792e5e19e21da6ede1e763d74d8
Henrique Canhadas <pedrosacanhadas@gmail.com>
2025-05-29 15:23:04 -0300

    att

--- setup/settings.py
+++ setup/settings.py
@@ -40,7 +40,8 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.dummy'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
